82c8607af0018b901cadd720d3e26d4abddf6dfd
Alena Zueva <aleo.zueva@gmail.com>
2020-06-05 12:36:19 +0700

    Amends code in main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict';
-var PRICES = [300, 400, 500, 600];
-var ROOMS = [1, 2, 3, 4];
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var GUESTS = [1, 2, 3, 4, 5, 6];
-var CHECKTIME = ['12:00', '13:00', '14:00'];
-var PICTURES = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var prices = [300, 400, 500, 600];
+var rooms = [1, 2, 3, 4];
+var types = ['palace', 'flat', 'house', 'bungalo'];
+var guests = [1, 2, 3, 4, 5, 6];
+var checkOptions = ['12:00', '13:00', '14:00'];
+var pictures = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
 
 var getRandomArrayIndex = function (array) {
@@ -13,8 +13,8 @@ var getRandomArrayIndex = function (array) {
   return Math.floor(Math.random() * Math.floor(arrayLength));
 };
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomInteger(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 var generateArray = function () {
@@ -28,19 +28,19 @@ var generateArray = function () {
       'offer': {
         'title': 'Some text',
         'address': '600, 350',
-        'price': PRICES[getRandomArrayIndex(PRICES)],
-        'type': TYPES[getRandomArrayIndex(TYPES)],
-        'rooms': ROOMS[getRandomArrayIndex(PRICES)],
-        'guests': GUESTS[getRandomArrayIndex(GUESTS)],
-        'checkin': CHECKTIME[getRandomArrayIndex(CHECKTIME)],
-        'checkout': CHECKTIME[getRandomArrayIndex(CHECKTIME)],
-        'features': FEATURES.slice(0, getRandomArrayIndex(FEATURES)),
+        'price': prices[getRandomArrayIndex(prices)],
+        'type': types[getRandomArrayIndex(types)],
+        'rooms': rooms[getRandomArrayIndex(rooms)],
+        'guests': guests[getRandomArrayIndex(guests)],
+        'checkin': checkOptions[getRandomArrayIndex(checkOptions)],
+        'checkout': checkOptions[getRandomArrayIndex(checkOptions)],
+        'features': features.slice(0, getRandomArrayIndex(features)),
         'description': 'Your room description',
-        'photos': PICTURES.slice(0, getRandomArrayIndex(PICTURES)),
+        'photos': pictures.slice(0, getRandomArrayIndex(pictures)),
       },
       'location': {
-        'x': getRandomArbitrary(0, 300),
-        'y': getRandomArbitrary(130, 630)
+        'x': getRandomInteger(0, 1000),
+        'y': getRandomInteger(130, 630)
       }
     };
     newArray.push(newItem);
@@ -59,12 +59,14 @@ var fragment = document.createDocumentFragment();
 
 var renderPin = function (item) {
   var pinElement = similarPinTemplate.cloneNode(true); /* клонируем структуру элемента, далее наполняем его новыми данными*/
-  // eslint-disable-next-line no-console
-  console.log(item);
-  pinElement.style.left = item.location.x;
-  pinElement.style.top = item.location.y;
-  pinElement.querySelector('img').alt = item.offer.description;
-  pinElement.querySelector('img').src = item.author.avatar;
+
+  pinElement.style.left = (item.location.x + 40) + 'px';
+  pinElement.style.top = (item.location.y + 44) + 'px';
+
+  var pinImage = pinElement.querySelector('img');
+  pinImage.alt = item.offer.description;
+  pinImage.src = item.author.avatar;
+
   return pinElement;
 };
 
